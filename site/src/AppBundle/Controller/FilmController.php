8f0961821de2e7a78ba655f934921c966c48ff5d
Kevin Mougammadaly <kevin.mougammadaly@gmail.com>
2018-03-01 11:25:18 +0100

    Updating template film and liste film

--- site/src/AppBundle/Controller/FilmController.php
+++ site/src/AppBundle/Controller/FilmController.php
@@ -8,21 +8,43 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Comment;
+use AppBundle\Form\CommentType;
+use AppBundle\Manager\CategoryManager;
+use AppBundle\Manager\CommentManager;
 use AppBundle\Manager\FilmManager;
+use AppBundle\Manager\SagaManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class FilmController extends Controller
 {
     private $manager;
+    /**
+     * @var SagaManager
+     */
+    private $sagaManager;
+    /**
+     * @var CommentManager
+     */
+    private $commentManager;
+    /**
+     * @var CategoryManager
+     */
+    private $categoryManager;
 
-    public function __construct(FilmManager $manager)
+    public function __construct(FilmManager $manager, SagaManager $sagaManager, CommentManager $commentManager, CategoryManager $categoryManager)
     {
         $this->manager = $manager;
+        $this->sagaManager = $sagaManager;
+        $this->commentManager = $commentManager;
+        $this->categoryManager = $categoryManager;
     }
 
     /**
-     * @Route("/films/{idPage}", name="films")
+     * @Route("/films/{idPage}", name="films", requirements={"idPage" = "\d+"})
      *
      * @param $idPage
      * @return \Symfony\Component\HttpFoundation\Response
@@ -30,11 +52,16 @@ class FilmController extends Controller
     public function filmsAction(int $idPage)
     {
         $nbPerPage = 3;
+        $categories = $this->categoryManager->getAllCategories();
+        $notes = $this->commentManager::NOTES;
+
         $data = $this->manager->getAllFilms($idPage, $nbPerPage);
         return $this->render('film/list_films.html.twig', [
-            'listFilms' => $data[0],
-            'nbPages' => $data[1],
-            'page' => $idPage,
+            'listFilms'  => $data[0],
+            'nbPages'    => $data[1],
+            'page'       => $idPage,
+            'categories' => $categories,
+            'notes'      => $notes
         ]);
     }
 
@@ -43,9 +70,36 @@ class FilmController extends Controller
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function filmAction($filmID)
+    public function filmAction(Request $request, $filmID)
     {
-        return $this->render('film/film.html.twig');
+        $film = $this->manager->getFilmById($filmID);
+
+        if (null === $film) {
+            throw new NotFoundHttpException("La page n'existe pas");
+        }
+
+        $comment = new Comment();
+        $comment->setUser($this->getUser());
+        $comment->setFilm($film);
+
+        $commentUser = $this->commentManager->getCommentByUser($film, $this->getUser());
+
+        if ($commentUser){
+            $comment = $commentUser;
+        }
+
+        $form = $this->createForm(CommentType::class,$comment);
+
+        if ($request->isMethod('POST') AND  $form->handleRequest($request)->isValid()){
+
+            $this->commentManager->addComment($comment);
+        }
+
+            return $this->render('film/film.html.twig',[
+                'myFilm'    =>  $film,
+                'form'      =>  $form->createView(),
+                'myComment' =>  $commentUser
+        ]);
     }
 
     /**
---
+++ site/src/AppBundle/Form/CommentType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('description',TextareaType::class)
+            ->add('note', ChoiceType::class, array(
+                'choices' => array(
+                '1' => '1',
+                '2' => '2',
+                '3' => '3',
+                '4' => '4',
+                '5' => '5',
+                '6' => '6',
+                '7' => '7',
+                '8' => '8',
+                '9' => '9',
+                '10'=> '10',
+            )))
+            ->add('valider', SubmitType::class)
+        ;
+
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Comment'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_comment';
+    }
+
+
+}
